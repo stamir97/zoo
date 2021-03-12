326a88a78921d3a6bd0e62b2428cb5bccfc26d8a
maireadreid <72817494+maireadreid@users.noreply.github.com>
2021-03-12 11:00:13 +0000

    Create populate.sql
    Create file and add initial data - will be changed later

---
+++ populate.sql
@@ -0,0 +1,18 @@
+use zoo;
+
+insert into
+enclosure
+(enclosure_id, enclosure_name, habitat, animal, location, hours)
+values
+(1, 'penguin parade', 'pool', 'penguin', 'hillside view', '0800-1800');
+
+INSERT INTO 
+ANIMALS
+(animal_id, animal_name, species_name, animal_DOB, arrival_date)
+VALUES
+(1, 'Alex', 'lion', '2001-02-28', '2000-02-28'),
+(2, 'Marty', 'zebra', '2004-05-23', '2006-03-08'),
+(3, 'Melman', 'giraffe', '2006-08-19', '2010-04-01'),
+(4, 'Gloria', 'hippopotamus', '2020-04-15', '2021-01-09'),
+(5, 'Skipper', 'penguin', '2018-09-23', '2020-11-15'),
+(6, 'Julien', 'lemur', '2016-12-16', '2016-12-16');
