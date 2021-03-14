625935b15cef6b0957d6ad488987e0d388873a95
stamir97 <62836957+stamir97@users.noreply.github.com>
2021-03-14 09:42:45 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -16,3 +16,26 @@ VALUES
 (4, 'Gloria', 'hippopotamus', '2020-04-15', '2021-01-09'),
 (5, 'Skipper', 'penguin', '2018-09-23', '2020-11-15'),
 (6, 'Julien', 'lemur', '2016-12-16', '2016-12-16');
+
+INSERT INTO feeding
+(FoodType_ID, FoodType, animal_id, Amount_eats(kg))
+VALUES 
+(1, beef, 1, 5),
+(2, grass, 2, 3),
+(3, acacia, 3, 3),
+(4, 'herbivore pellets', 4, 7),
+(5, fish, 5, 10),
+(6, insects, 6, 12);
+
+INSERT INTO stock
+(FoodType_ID, Amount_available Float)
+VALUES
+(1, 10),
+(2, 1),
+(3, 0),
+(4, 15),
+(5, 10),
+(6, 20);
+                                               
+                                            
+  
